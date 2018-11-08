09310c1a1f0ea6d34d895524be2987baaf8cfc03
felix <madwang@tencent.com>
2018-11-08 22:10:11 +0800

    ...

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
 # coding=utf-8
 
 import sys
+import os
+
+
+def load_dict(file_path):
+    words = set([])
+    with open(file_path, 'r') as fin:
+        for line in fin:
+            if len(line.strip()) > 0:
+                words.add(line.strip())
+    return words
+
+def is_stopword(term):
+    return (term.lower() in stopwords)
 
 def is_chinese(uchar):
     if 0x4e00 <= ord(uchar) <= 0x9fff:
